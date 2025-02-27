79b56f820cc1bd4e0791c83f010a51a1d6497690
gdsfaww <ant.khakimov@yandex.ru>
2025-02-27 10:55:37 +0300

    Обновлены стили в globals.css и удалены ненужные файлы

---
+++ src/app/components/InterestingThesis.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+// Простой функциональный компонент "InterestingThesis"
+// Добавьте содержимое и стили по необходимости
+const InterestingThesis: React.FC = () => {
+  return (
+    <section>
+      <h2>Интересная теза</h2>
+      <p>Здесь может быть какая-либо полезная информация или тезис, который заинтересует пользователя.</p>
+    </section>
+  );
+};
+
+export default InterestingThesis; 
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,7 @@
-import Header from './components/Header';
 import HeroSection from './components/HeroSection';
 import Statistics from './components/Statistics';
 import CaseStudy from './components/CaseStudy';
 import FAQ from './components/FAQ';
-import Footer from './components/Footer';
-import InterestingThesis from './components/InterestingThesis';
-import OwnerNeeds from './components/OwnerNeeds';
-import FinancialOffer from './components/FinancialOffer';
-import WorkProcess from './components/WorkProcess';
-import Agreements from './components/Agreements';
-import ContentBlock from './components/ContentBlock';
-import LeadMagnet from './components/LeadMagnet';
 
 export default function Home() {
   return (
